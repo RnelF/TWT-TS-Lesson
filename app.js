c8a6c54b6539402368f6b63ebd0c82d6df11a498
Arnel Francia <nelffff@gmail.com>
2024-10-16 19:57:39 +0800

    Enums

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
 "use strict";
-let direction;
-direction = "south";
+var Size;
+(function (Size) {
+    Size[Size["Smallest"] = 1] = "Smallest";
+    Size[Size["Medium"] = 2] = "Medium";
+    Size[Size["Large"] = 3] = "Large";
+})(Size || (Size = {}));
+var size = Size.Smallest;
+if (size === Size.Smallest) {
+}
+//String Enums
+var Direction;
+(function (Direction) {
+    Direction["Up"] = "UP";
+    Direction["Down"] = "DOWN";
+    Direction["Left"] = "LEFT";
+    Direction["Right"] = "RIGHT";
+})(Direction || (Direction = {}));
+var value;
--- app.ts
+++ app.ts
@@ -1,2 +1,21 @@
-let direction: string;
-direction = "south";
+enum Size {
+  Smallest = 1,
+  Medium,
+  Large,
+}
+
+var size: Size = Size.Smallest;
+
+if (size === Size.Smallest) {
+}
+
+//String Enums
+
+enum Direction {
+  Up = "UP",
+  Down = "DOWN",
+  Left = "LEFT",
+  Right = "RIGHT",
+}
+
+var value: Direction;
