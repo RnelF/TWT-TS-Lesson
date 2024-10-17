6b6e7ddad91c3ec7b62ee681d6184eba74633c13
Arnel Francia <nelffff@gmail.com>
2024-10-17 19:38:45 +0800

    ? and ! operator

--- app.js
+++ app.js
@@ -1,25 +1,4 @@
 "use strict";
-function processFeedback(input) {
-    //assume we can perform any operation without explicit type checks
-    console.log(`Processing: ${input}`);
-    //further processing logic
-}
-//This can be a tring, number,file,etc
-processFeedback("Great service!");
-processFeedback(5);
-processFeedback(new Blob());
-function processingInfo(input) {
-    if (typeof input === "string") {
-        console.log(`Processing Text: ${input}`);
-    }
-    else if (typeof input === "number") {
-        console.log(`Processing Rating: ${input}`);
-    }
-    else if (input instanceof Blob) {
-        console.log(`Processing file`);
-    }
-    else {
-        console.log(`Unsupported type of Input`);
-    }
-}
-processingInfo(25);
+var _a;
+const arr = [{ name: "tim" }, { name: "joe" }, { name: "jane" }];
+const el = (_a = arr.pop()) === null || _a === void 0 ? void 0 : _a.name;
--- app.ts
+++ app.ts
@@ -1,24 +1,3 @@
-function processFeedback(input: any): void {
-  //assume we can perform any operation without explicit type checks
-  console.log(`Processing: ${input}`);
-  //further processing logic
-}
+const arr = [{ name: "tim" }, { name: "joe" }, { name: "jane" }];
 
-//This can be a tring, number,file,etc
-processFeedback("Great service!");
-processFeedback(5);
-processFeedback(new Blob());
-
-function processingInfo(input: unknown): void {
-  if (typeof input === "string") {
-    console.log(`Processing Text: ${input}`);
-  } else if (typeof input === "number") {
-    console.log(`Processing Rating: ${input}`);
-  } else if (input instanceof Blob) {
-    console.log(`Processing file`);
-  } else {
-    console.log(`Unsupported type of Input`);
-  }
-}
-
-processingInfo(25);
+const el = arr.pop()?.name;
