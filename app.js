529ed366693610fb91b28ae62c42c47940910efa
Arnel Francia <nelffff@gmail.com>
2024-10-17 19:59:26 +0800

    function types

--- app.js
+++ app.js
@@ -1,4 +1,18 @@
 "use strict";
-var _a;
-const arr = [{ name: "tim" }, { name: "joe" }, { name: "jane" }];
-const el = (_a = arr.pop()) === null || _a === void 0 ? void 0 : _a.name;
+function add(x, y) {
+    return x + y;
+}
+const result = add(1, 2);
+console.log(result);
+function makeName(firstName, lastName, middleName) {
+    if (middleName)
+        return firstName + " " + middleName + " " + lastName;
+    return firstName + " " + lastName;
+}
+const fullName = makeName("Arnel", "Francia", "Yubal");
+console.log(fullName);
+///////////////////////////////
+function callFunc(func, param1, param2) {
+    func(param1, param2);
+}
+callFunc(makeName, "Arnel", "Francia");
--- app.ts
+++ app.ts
@@ -1,3 +1,29 @@
-const arr = [{ name: "tim" }, { name: "joe" }, { name: "jane" }];
+function add(x: number, y: number) {
+  return x + y;
+}
 
-const el = arr.pop()?.name;
+const result = add(1, 2);
+
+console.log(result);
+
+function makeName(firstName: string, lastName: string, middleName?: string) {
+  if (middleName) return firstName + " " + middleName + " " + lastName;
+
+  return firstName + " " + lastName;
+}
+
+const fullName = makeName("Arnel", "Francia", "Yubal");
+
+console.log(fullName);
+
+///////////////////////////////
+
+function callFunc(
+  func: (f: string, l: string, m?: string) => string,
+  param1: string,
+  param2: string
+) {
+  func(param1, param2);
+}
+
+callFunc(makeName, "Arnel", "Francia");
