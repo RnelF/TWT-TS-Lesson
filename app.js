9d1a74e3bf95487a18229ef32131c8c8cf88ab89
Arnel Francia <nelffff@gmail.com>
2024-10-18 19:28:17 +0800

    rest parameters

--- app.js
+++ app.js
@@ -1,20 +1,6 @@
 "use strict";
-function mul(x, y) {
-    return x * y;
+function sum(...numbers) {
+    return console.log(numbers);
 }
-function div(x, y) {
-    return x / y;
-}
-function applyFunc(funcs, value) {
-    const results = [];
-    for (let i = 0; (i = funcs.length); i++) {
-        const args = value[i];
-        const result = funcs[i](args[0], args[1]);
-        results.push(result);
-    }
-    return results;
-}
-applyFunc([mul, div], [
-    [1, 2],
-    [4, 5],
-]);
+sum(1, 2, 3);
+sum();
--- app.ts
+++ app.ts
@@ -1,28 +1,6 @@
-function mul(x: number, y: number): number {
-  return x * y;
+function sum(...numbers: number[]) {
+  return console.log(numbers);
 }
 
-function div(x: number, y: number): number {
-  return x / y;
-}
-
-function applyFunc(
-  funcs: ((x: number, y: number) => number)[],
-  value: [number, number][]
-): number[] {
-  const results: number[] = [];
-  for (let i = 0; (i = funcs.length); i++) {
-    const args = value[i];
-    const result = funcs[i](args[0], args[1]);
-    results.push(result);
-  }
-  return results;
-}
-
-applyFunc(
-  [mul, div],
-  [
-    [1, 2],
-    [4, 5],
-  ]
-);
+sum(1, 2, 3);
+sum();
