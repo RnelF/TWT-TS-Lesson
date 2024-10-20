1070c8fd63ff5ef2815ef4ed58d890df871e4dee
Arnel Francia <nelffff@gmail.com>
2024-10-20 19:08:04 +0800

    practicing abstract classes

--- app.js
+++ app.js
@@ -1,14 +1,56 @@
 "use strict";
+var animalActions;
+(function (animalActions) {
+    animalActions["Run"] = "Runs";
+    animalActions["Jump"] = "Jumps";
+    animalActions["Lay"] = "Layed Down";
+})(animalActions || (animalActions = {}));
 class Animal {
-    move(duration) {
+    move(duration, act) {
         console.log("Moving along...");
         this.makeSound(duration);
+        this.makeAnotherSound(duration);
+        this.doAnAction(act);
     }
 }
 class Dog extends Animal {
     makeSound(duration) {
         console.log("Woof Woof");
     }
+    makeAnotherSound(duration) {
+        console.log("Grrrrrrrrr");
+    }
+    doAnAction(act) {
+        if (act === animalActions.Jump) {
+            console.log(`The Dog ${act}`);
+        }
+        else if (act === animalActions.Run) {
+            console.log(`The Dog ${act}`);
+        }
+        else if (act === animalActions.Lay) {
+            console.log(`The Dog ${act}`);
+        }
+    }
+}
+class Cow extends Animal {
+    makeSound(duration) {
+        console.log("Moo Moo");
+    }
+    makeAnotherSound(duration) {
+        console.log("Thud Thud");
+    }
+    doAnAction(act) {
+        if (act === animalActions.Jump) {
+            console.log(`The Cow ${act}`);
+        }
+        else if (act === animalActions.Run) {
+            console.log(`The Cow ${act}`);
+        }
+        else if (act === animalActions.Lay) {
+            console.log(`The Cow ${act}`);
+        }
+    }
 }
 const dog = new Dog();
-dog.move(10);
+const cow = new Cow();
+cow.move(10, animalActions.Jump);
--- app.ts
+++ app.ts
@@ -1,9 +1,19 @@
+enum animalActions {
+  "Run" = "Runs",
+  "Jump" = "Jumps",
+  "Lay" = "Layed Down",
+}
+
 abstract class Animal {
   abstract makeSound(duration: number): void;
+  abstract makeAnotherSound(duration: number): void;
+  abstract doAnAction(act: animalActions): void;
 
-  move(duration: number) {
+  move(duration: number, act: animalActions) {
     console.log("Moving along...");
     this.makeSound(duration);
+    this.makeAnotherSound(duration);
+    this.doAnAction(act);
   }
 }
 
@@ -11,8 +21,40 @@ class Dog extends Animal {
   makeSound(duration: number): void {
     console.log("Woof Woof");
   }
+  makeAnotherSound(duration: number): void {
+    console.log("Grrrrrrrrr");
+  }
+  doAnAction(act: animalActions): void {
+    if (act === animalActions.Jump) {
+      console.log(`The Dog ${act}`);
+    } else if (act === animalActions.Run) {
+      console.log(`The Dog ${act}`);
+    } else if (act === animalActions.Lay) {
+      console.log(`The Dog ${act}`);
+    }
+  }
+}
+
+class Cow extends Animal {
+  makeSound(duration: number): void {
+    console.log("Moo Moo");
+  }
+
+  makeAnotherSound(duration: number): void {
+    console.log("Thud Thud");
+  }
+  doAnAction(act: animalActions): void {
+    if (act === animalActions.Jump) {
+      console.log(`The Cow ${act}`);
+    } else if (act === animalActions.Run) {
+      console.log(`The Cow ${act}`);
+    } else if (act === animalActions.Lay) {
+      console.log(`The Cow ${act}`);
+    }
+  }
 }
 
 const dog = new Dog();
+const cow = new Cow();
 
-dog.move(10);
+cow.move(10, animalActions.Jump);
