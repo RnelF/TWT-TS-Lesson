c93e1f4fe811934adcf20a1b279539f5e79a375e
Arnel Francia <nelffff@gmail.com>
2024-10-24 19:46:43 +0800

    updated

--- app.js
+++ app.js
@@ -1,73 +1,9 @@
 "use strict";
-const carValue = [
-    "A",
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-    "10",
-    "J",
-    "Q",
-    "K",
-];
-const cardSuits = ["Heart", "Diamond", "Clove", "Spade"];
-const createDeck = () => {
-    const deck = [];
-    for (let suit of cardSuits) {
-        for (let value of carValue) {
-            deck.push(`${value} of ${suit}`);
-        }
+function addContact(contact) {
+    if ("birthday" in contact) {
+        console.log(contact.name, contact.birthday);
     }
-    return deck;
-};
-const shuffleDeck = (deck) => {
-    for (let i = deck.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [deck[i], deck[j]] = [deck[j], deck[i]]; // Swap elements
+    else {
+        console.log(contact.companyName, contact.workPhone);
     }
-    return deck;
-};
-let deck = createDeck();
-deck = shuffleDeck(deck);
-const handedCards = () => {
-    const card1 = deck[0];
-    const card2 = deck[1];
-    const hand = [card1, card2];
-    return hand;
-};
-let playerHand = handedCards();
-console.log(playerHand);
-const playerHit = () => {
-    const hitCard = deck[2];
-    playerHand.push(hitCard);
-};
-playerHit();
-console.log(playerHand);
-const playerHandSum = () => {
-    let sum = 0;
-    let aceCount = 0;
-    for (let card of playerHand) {
-        const value = card.split(" ")[0]; // Get the value part of the card
-        if (value === "A") {
-            aceCount += 1;
-            sum += 11; // Count Aces as 11 initially
-        }
-        else if (["J", "Q", "K"].includes(value)) {
-            sum += 10; // Face cards (J, Q, K) are worth 10
-        }
-        else {
-            sum += parseInt(value); // Number cards are worth their face value
-        }
-    }
-    // Adjust for Aces if the sum is over 21
-    while (sum > 21 && aceCount > 0) {
-        sum -= 10;
-        aceCount -= 1;
-    }
-    return sum;
-};
-console.log(playerHandSum());
+}
--- app.ts
+++ app.ts
@@ -1,88 +1,21 @@
-const carValue: string[] = [
-  "A",
-  "2",
-  "3",
-  "4",
-  "5",
-  "6",
-  "7",
-  "8",
-  "9",
-  "10",
-  "J",
-  "Q",
-  "K",
-];
+interface Individual {
+  name: string;
+  birthday: Date;
+}
 
-const cardSuits: string[] = ["Heart", "Diamond", "Clove", "Spade"];
+interface Organization {
+  companyName: string;
+  workPhone: string;
+}
 
-const createDeck = () => {
-  const deck: string[] = [];
-  for (let suit of cardSuits) {
-    for (let value of carValue) {
-      deck.push(`${value} of ${suit}`);
-    }
+type ContactType = Individual | Organization;
+
+type CompContact = Individual & Organization;
+
+function addContact(contact: ContactType) {
+  if ("birthday" in contact) {
+    console.log(contact.name, contact.birthday);
+  } else {
+    console.log(contact.companyName, contact.workPhone);
   }
-  return deck;
-};
-
-const shuffleDeck = (deck: string[]) => {
-  for (let i = deck.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [deck[i], deck[j]] = [deck[j], deck[i]]; // Swap elements
-  }
-  return deck;
-};
-
-let deck = createDeck();
-deck = shuffleDeck(deck);
-
-const handedCards = (): string[] => {
-  const card1: string = deck[0];
-  const card2: string = deck[1];
-
-  const hand: string[] = [card1, card2];
-
-  return hand;
-};
-
-let playerHand: string[] = handedCards();
-
-console.log(playerHand);
-
-const playerHit = (): void => {
-  const hitCard: string = deck[2];
-  playerHand.push(hitCard);
-};
-
-playerHit();
-
-console.log(playerHand);
-
-const playerHandSum = (): number => {
-  let sum = 0;
-  let aceCount = 0;
-
-  for (let card of playerHand) {
-    const value = card.split(" ")[0]; // Get the value part of the card
-
-    if (value === "A") {
-      aceCount += 1;
-      sum += 11; // Count Aces as 11 initially
-    } else if (["J", "Q", "K"].includes(value)) {
-      sum += 10; // Face cards (J, Q, K) are worth 10
-    } else {
-      sum += parseInt(value); // Number cards are worth their face value
-    }
-  }
-
-  // Adjust for Aces if the sum is over 21
-  while (sum > 21 && aceCount > 0) {
-    sum -= 10;
-    aceCount -= 1;
-  }
-
-  return sum;
-};
-
-console.log(playerHandSum());
+}
