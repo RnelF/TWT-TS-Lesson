16afd08469071f71bc63711467a720a4c741e38b
Arnel Francia <nelffff@gmail.com>
2024-10-27 19:51:43 +0800

    Pick utility type practice code

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
 "use strict";
-const todo = {
-    title: "Wake UP!",
+const newTodo = {
+    id: 123,
+    title: "Task 1",
+    description: "Clean bedroom",
     completed: false,
 };
+const updateTodo = (todo) => {
+    newTodo.title = todo.title;
+    newTodo.description = todo.description;
+    console.log(`Todo title and description updated!`);
+    if (todo.title === newTodo.title && todo.title === newTodo.description) {
+        console.log(`Nothing Changes`);
+    }
+    else if (todo.title || todo.description) {
+        console.log("todo Updated!");
+    }
+};
+updateTodo({ title: "Task 1", description: "clean kitchen" });
--- app.ts
+++ app.ts
@@ -1,12 +1,31 @@
 interface Todo {
   id: number;
   title: string;
+  description: string;
   completed: boolean;
 }
 
-type TodoPreview = Pick<Todo, "title" | "completed">;
+type TodoUpdatable = Pick<Todo, "title" | "description">;
 
-const todo: TodoPreview = {
-  title: "Wake UP!",
+const newTodo: Todo = {
+  id: 123,
+  title: "Task 1",
+  description: "Clean bedroom",
   completed: false,
 };
+
+const updateTodo = (todo: TodoUpdatable): void => {
+  newTodo.title = todo.title;
+
+  newTodo.description = todo.description;
+
+  console.log(`Todo title and description updated!`);
+
+  if (todo.title === newTodo.title && todo.title === newTodo.description) {
+    console.log(`Nothing Changes`);
+  } else if (todo.title || todo.description) {
+    console.log("todo Updated!");
+  }
+};
+
+updateTodo({ title: "Task 1", description: "clean kitchen" });
