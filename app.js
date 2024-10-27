2dcce28f32b9414e8d272da1198c595d46e0d6bc
Arnel Francia <nelffff@gmail.com>
2024-10-27 19:14:51 +0800

    readonly utility type

--- app.js
+++ app.js
@@ -1,15 +1,2 @@
 "use strict";
-const newTodo = {
-    title: "Wake up!",
-    description: "You are dreaming",
-};
-const updateTodo = (todo) => {
-    if (todo.description) {
-        newTodo.description = todo.description;
-    }
-    else if (todo.title) {
-        newTodo.title = todo.title;
-    }
-};
-updateTodo({ title: "Good Morning!" });
-console.log(newTodo);
+const myTodo = { title: "Learn TS!" };
--- app.ts
+++ app.ts
@@ -1,20 +1,5 @@
 interface Todo {
   title: string;
-  description: string;
 }
 
-const newTodo: Todo = {
-  title: "Wake up!",
-  description: "You are dreaming",
-};
-
-const updateTodo = (todo: Partial<Todo>): void => {
-  if (todo.description) {
-    newTodo.description = todo.description;
-  } else if (todo.title) {
-    newTodo.title = todo.title;
-  }
-};
-updateTodo({ title: "Good Morning!" });
-
-console.log(newTodo);
+const myTodo: Readonly<Todo> = { title: "Learn TS!" };
